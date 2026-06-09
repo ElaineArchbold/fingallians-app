68a0eb40fa18e8f670c5f3e68f1822ff1c8e95a9
ElaineArchbold <e.t.archbold@gmail.com>
2026-06-09 14:04:27 +0100

    removed space from tandcs

--- src/App.jsx
+++ src/App.jsx
@@ -523,7 +523,7 @@ function AuthScreen({ showToast }) {
                 </div>
                 <div className="tc-section">
                   <strong>Data & Privacy</strong>
-                  <p>To use this app we store your child's first and last name and your email address. No other personal information is collected or stored. Your data is not shared with any third party and is used solely to manage participation in the 2026 Summer Challenge. You can request deletion of your data at any time by emailing <strong>fingallians2014boys@gmail.com</strong>.</p>
+                  <p>To use this app we store your child's first and last name and your email address. No other personal information is collected or stored. Your data is not shared with any third party and is used solely to manage participation in the 2026 Summer Challenge. You can request deletion of your data at any time by emailing fingallians2014boys@gmail.com.</p>
                 </div>
                 <div className="tc-section">
                   <strong>Participation</strong>
